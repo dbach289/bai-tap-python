8cec3ba48b444d4872493c5eaf0eb29defa43fa9
Junny <dbach289@gmail.com>
2026-03-29 15:15:31 +0700

    update: reorganize files

--- BTTL/2_3_bai1.py
+++
@@ -1,9 +0,0 @@
-# Nhập 2 số nguyên từ bàn phím
-a = int(input("Nhập số a: "))
-b = int(input("Nhập số b: "))
-
-# Tính tổng
-tong = a + b
-
-# In kết quả
-print("Tổng của hai số là:", tong)
--- vi du/toantulogic.py
+++
@@ -1,2 +0,0 @@
-ketqua = (6<8) and (5>9) and (4**4> 100) and (7 != 8)
-print(ketqua)
